72139fb5fe2ff9f77bf25ba7294b8cea0f531025
HafidZiti <ziti.hafid@gmail.com>
2018-03-19 22:25:30 +0100

    User login --> Database Firebase

--- src/providers/authservice/authservice.ts
+++ src/providers/authservice/authservice.ts
@@ -13,56 +13,32 @@ export class AuthserviceProvider {
   }
 
   saveUser(_user: UserProfile) {
-    // if (this.checkUserIfArealySaved(_user)) {
-
-    this.checkUserIfArealySaved(_user);
-    this.db.list('Users/').set(_user.uid, _user);
-
-   // this.checkUserIfArealySaved(_user);
-
-    // this.users = this.db.list('Users/');
-    // const Ref$ = this.users.push(<UserProfile>{});
-    // _user.key = Ref$.key;
-
-
-    // }else console.log('user deja existe');
+    if (this.checkUserIfArealySaved(_user).length == 0) {
+      this.db.list('Users/').set(_user.uid, _user);
+    } else console.log('user deja existe');
   }
 
-  private checkUserIfArealySaved(user: UserProfile): AngularFireObject<UserProfile> {
-    // const path = 'Users/'.concat(user.uid).concat('/').concat(user.uid);
+
+  private checkUserIfArealySaved(user: UserProfile): any {
     const path = 'Users/';
     console.log('ptah', path);
     const ref$ = this.db.list(path, ref => ref.orderByChild('email').equalTo(user.email));
+    ref$.valueChanges().subscribe(data => {
+      console.log('la taille de l data', data.length);
+      return data;
+      // data.map((t:UserProfile)  => {
+      //   console.log('objet returne',t.email);
+      // })
+    })
 
     //  ref$.snapshotChanges().subscribe(data=>{
     //    console.log('la taile de la reposne',data)
     // })
 
-
-    ref$.valueChanges().subscribe(data => {
-      console.log('la taile de la reposne', data.length);
-
-
-      // for (let user:UserProfile of data){
-      //   console.log('objet returne',user.email);
-      // }
-
-      data.map((t:UserProfile)  => {
-       // t.valueOf().hasOwnProperty('email')
-        //con
-        console.log('objet returne',t.email);
-      })
-    })
-
-
-
-    return null;
+  }
+}
 
 
 // this.item = af.database.object('/item', { preserveSnapshot: true });
 // this.item.subscribe(snapshot => {
 //   this.values= snapshot.val();
-
-
-  }
-}
